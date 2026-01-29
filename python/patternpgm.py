2c04094370684956eae5a3c83662a7ea45c7a433
Rutvik Mathapati <ruties4354@gmail.com>
2026-01-29 10:11:25 +0530

    29jan

---
+++ python/patternpgm.py
@@ -0,0 +1,43 @@
+"""
+1.Write a Python program to print a square or rectangle pattern using asterisks (*).
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+
+rows = 4
+cols = 5
+for i in range(rows):
+    for j in range(cols):
+        print("*",end=" ")
+    print()
+"""
+
+'''
+#2. WAP pgm to print right angled trinage pattern using asterisks (*).
+*
+* *
+* * *
+* * * *
+* * * * *
+
+rows = 5
+for i in range(rows):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+'''
+'''
+#3 WAP pgm to print inverted right angled triangle pattern using asterisks (*).
+* * * * *
+* * * * 
+* * *
+* *
+*
+rows = 5
+for i in range(rows):
+    for j in range(rows-i):
+        print("*",end=" ")
+    print()
+'''
---
+++ python/pgms/output/prac.py
@@ -0,0 +1,68 @@
+"""lst = [10,2.5,'hi']
+print(lst)
+lst[1]=90
+print(lst)
+lst.append('hello')
+print(lst)"""
+"""
+range(10)
+print(list(range(10)))
+range(3,10)
+print(list(range(3,10)))
+range(3,10,2)
+print(list(range(3,10,2)))
+a=list(range(10,3,-1))
+print(list(range(10,3,-1)))
+print(a)"""
+
+"""n=5
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i):
+        print("* ",end="")
+    print()
+    
+n=5
+for i in range(1,n+1):
+    print(" "*(i-1),end="")
+    for j in range(n-i+1):
+        print("* ",end="")
+    print()
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+n = 5
+for i in range(n):
+    for j in range(i, n-1):   # spaces
+        print(' ', end=' ')
+    for j in range(i+1):      # stars
+        print('*', end=' ')
+    print()
+
+
+n = 5
+for i in range(n):
+    for j in range(i, n-1):
+        print(' ', end=' ')
+    for j in range(i):
+        print('*', end=' ')
+    for j in range(i+1):
+        print('*', end=' ')
+    print()
+"""
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    for j in range(i+1,n):
+        print("*",end=" ")
+    print() 
